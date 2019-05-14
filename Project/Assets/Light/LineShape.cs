662092fca7e11bf4b9fbef3a913063eb2147966d
billwillman <billwillman@qq.com>
2019-05-14 20:02:03 +0800

    11

--- Project/Assets/Light/LineShape.cs
+++ Project/Assets/Light/LineShape.cs
@@ -4,6 +4,20 @@ using System.Collections;
 public class LineShape : MonoBehaviour, ILightShape {
 
 	public Vector3 p0, p1;
+    public BoxCollider Bind = null;
+
+    void Awake() {
+        if (Bind != null) {
+            var b = Bind.bounds;
+            float x = (b.max.x - b.min.x) / 2f;
+            float z = (b.max.z - b.min.z) / 2f;
+            Vector3 center = b.center;
+            center.y = 0;
+            Vector3 dir = new Vector3(x, 0, z);
+            p0 = center - dir;
+            p1 = center + dir;
+        }
+    }
 
 	public bool IsVaild
 	{
@@ -17,9 +31,12 @@ public class LineShape : MonoBehaviour, ILightShape {
 	{
 		if ((shadowMesh == null) || (light == null))
 			return;
-		if (!IsVaild) {
-			shadowMesh.Clear ();
+
+        shadowMesh.Clear();
+        if (!IsVaild) {
 			return;
 		}
+
+
 	}
 }
