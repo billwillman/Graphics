24a7719f643d9eed787651bffb185b00d7611091
billwillman <billwillman@qq.com>
2019-07-08 15:52:46 +0800

    11

---
+++ Project/Assets/Script/SignFieldDistanceShadow/sdGameObj.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sdGameObj : MonoBehaviour
+{
+    private int m_Mat_Pos = 0;
+
+    protected int Material_Pos {
+        get {
+            if (m_Mat_Pos == 0)
+                m_Mat_Pos = Shader.PropertyToID("pos");
+            return m_Mat_Pos;
+        }
+    }
+
+   public virtual bool Attach(Material mat) {
+        if (mat == null)
+            return false;
+        if (!mat.HasProperty(Material_Pos)) {
+            Debug.LogErrorFormat("material property pos not found");
+            return false;
+        }
+        Vector4 pos = this.transform.position;
+        mat.SetVector(Material_Pos, pos);
+        return true;
+    }
+}
---
+++ Project/Assets/Script/SignFieldDistanceShadow/sdSphere.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sdSphere : sdGameObj {
+    public float m_Radius = 1f;
+    private int m_Mat_Radius = 0;
+
+    protected int Material_Radius {
+        get {
+            if (m_Mat_Radius == 0) {
+                m_Mat_Radius = Shader.PropertyToID("radius");
+            }
+            return m_Mat_Radius;
+        }
+    }
+
+    public override bool Attach(Material mat) {
+        bool ret = base.Attach(mat);
+        if (!ret)
+            return ret;
+        ret = mat.HasProperty(Material_Radius);
+        if (!ret)
+            return ret;
+        mat.SetFloat(Material_Radius, m_Radius);
+        return true;
+    }
+}
