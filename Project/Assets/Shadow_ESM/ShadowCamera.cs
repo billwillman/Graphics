85c9c53252eefd7a7559487c5b36cdca9c00b277
billwillman <billwillman@qq.com>
2020-03-30 20:53:36 +0800

    1

---
+++ Project/Assets/Shadow_ESM/ShadowCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ShadowCamera : MonoBehaviour
+{
+
+    private Camera m_Cam = null;
+
+    private void Start() {
+        m_Cam = Camera.main;
+        if (m_Cam != null) {
+
+        }
+    }
+}
